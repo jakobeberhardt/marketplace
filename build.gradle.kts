25db81bef482b67845b4cbed273e8c1363409d72
jakobeberhardt <jakob.eberhardt@mail.de>
2022-10-20 20:32:53 +0200

    Add settings for SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+    id("org.sonarqube") version "3.4.0.2513"
 }
 
 group = "de.neocargo"
