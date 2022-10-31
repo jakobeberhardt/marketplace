d0ea53b77728bdb13e75a56f4f5fb699a788e38b
Kevin Schwab <scke1027@h-ka.de>
2022-10-31 16:05:37 +0100

    Add cors policy

--- src/main/kotlin/de/neocargo/marketplace/controller/ContractController.kt
+++ src/main/kotlin/de/neocargo/marketplace/controller/ContractController.kt
@@ -5,6 +5,7 @@ import de.neocargo.marketplace.entity.Shipment
 import de.neocargo.marketplace.repository.ContractRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin()
 @RequestMapping("/contracts")
 class ContractController(
     private val contractRepository: ContractRepository
