35303cafe9060d4030920b868546672434f1e987
Kevin Schwab <scke1027@h-ka.de>
2022-11-02 18:30:04 +0100

    Add JsonPropertyOrder to AllowedTimeWindow

--- src/main/kotlin/de/neocargo/marketplace/entity/AllowedTimeWindow.kt
+++ src/main/kotlin/de/neocargo/marketplace/entity/AllowedTimeWindow.kt
@@ -1,7 +1,12 @@
 package de.neocargo.marketplace.entity
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import java.util.Date
 
+@JsonPropertyOrder(
+    "startTime",
+    "endTime",
+)
 data class AllowedTimeWindow(
     val startTime: Date,
     val endTime: Date,
