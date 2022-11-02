0ad3319fca0d8eaff364b7445fe24e923c0a5155
Kevin Schwab <scke1027@h-ka.de>
2022-11-02 18:31:58 +0100

    Add JsonPropertyOrder to IntegerRequirement

--- src/main/kotlin/de/neocargo/marketplace/entity/IntegerRequirement.kt
+++ src/main/kotlin/de/neocargo/marketplace/entity/IntegerRequirement.kt
@@ -1,5 +1,11 @@
 package de.neocargo.marketplace.entity
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder(
+    "scopes",
+    "params",
+)
 data class IntegerRequirement(
     val scopes: Scopes,
     val params: String,
