f44adbedd2aa576a39b4c3c5b9638c74104b938d
Kevin Schwab <scke1027@h-ka.de>
2022-11-02 18:31:42 +0100

    Add JsonPropertyOrder to EnumRequirement

--- src/main/kotlin/de/neocargo/marketplace/entity/EnumRequirement.kt
+++ src/main/kotlin/de/neocargo/marketplace/entity/EnumRequirement.kt
@@ -1,5 +1,11 @@
 package de.neocargo.marketplace.entity
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder(
+    "scopes",
+    "params",
+)
 data class EnumRequirement(
     val scopes: Scopes,
     val params: String?,
