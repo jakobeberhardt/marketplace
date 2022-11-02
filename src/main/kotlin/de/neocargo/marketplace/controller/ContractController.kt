cce40d7488c9bee9088f9b1780b2ee141f66de86
Kevin Schwab <scke1027@h-ka.de>
2022-11-02 12:58:59 +0100

    Add ktlint and fix style issues

--- src/main/kotlin/de/neocargo/marketplace/controller/ContractController.kt
+++ src/main/kotlin/de/neocargo/marketplace/controller/ContractController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin()
+@CrossOrigin
 @RequestMapping("/contracts")
 class ContractController(
     private val contractRepository: ContractRepository
