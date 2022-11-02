488051b250b744b9d2807d7235724bcb96170db2
Kevin Schwab <scke1027@h-ka.de>
2022-11-02 18:32:28 +0100

    Add JsonPropertyOrder to GpsCoordinates

--- src/main/kotlin/de/neocargo/marketplace/entity/GpsCoordinates.kt
+++ src/main/kotlin/de/neocargo/marketplace/entity/GpsCoordinates.kt
@@ -1,5 +1,11 @@
 package de.neocargo.marketplace.entity
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder(
+    "latidute",
+    "longtidude",
+)
 data class GpsCoordinates(
     val latidute: Double,
     val longtidude: Double,
